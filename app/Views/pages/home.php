de205de2444d54090105e2377f09b553f0c0adda
1913224 <83791936+1913224@users.noreply.github.com>
2022-04-08 04:44:26 +0100

    Add files via upload

---
+++ app/Views/pages/home.php
@@ -0,0 +1,36 @@
+<section>
+    <div class="container">
+        <div id="MyCarousels" class="carousel slide" data-ride="carousel"> 
+            <div class="carousel-indicators bg-transparent">
+                    <button type="button" data-bs-target="#MyCarousels" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#MyCarousels" data-bs-slide-to="1" aria-label="slide 2"></button>
+                    <button type="button" data-bs-target="#MyCarousels" data-bs-slide-to="2" aria-label="slide 0"></button>
+                </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?=base_url('images/D2.jpg')?>" alt="D2">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?=base_url('images/WC.jpg')?>" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?=base_url('images/sr16.jpg')?>" alt="Third slide">
+                </div>
+            </div>
+            <button class="carousel-control-prev" data-bs-target="#MyCarousels" type="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon bg-transparent" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#MyCarousels" data-bs-slide="next">
+                    <span class="carousel-control-next-icon bg-transparent" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <div class="container">
+        <h1>Welcome to the Warmind Collective</h1>
+        <p>This is the archive collection of lore that has been written and uploaded by bungie for Destiny and Destiny 2 the games, this is for the lore readers and for people
+            to understand what is going on with the world of destiny.
+        </p>
+    </div>
+</section>
